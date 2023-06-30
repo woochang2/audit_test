d9a70f82dfa21793508bbdd071ead7ef091c23d3
wcjeong <wcjeong@postech.ac.kr>
2023-06-30 14:21:33 +0900

    first commit

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -16,7 +16,7 @@ def local(ctx, debug=True):
         'faults': 0,
         'nodes': 4,
         'workers': 1,
-        'rate': 50_000,
+        'rate': 5_000,
         'tx_size': 512,
         'duration': 20,
     }
@@ -26,7 +26,7 @@ def local(ctx, debug=True):
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 10_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
-        'batch_size': 500_000,  # bytes
+        'batch_size': 51_200,  # bytes
         'max_batch_delay': 200  # ms
     }
     try:
